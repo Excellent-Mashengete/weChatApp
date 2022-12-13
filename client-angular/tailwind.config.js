c009513fbfef06d781059e72162bfd2890727a08
Excellent <excellent.mashengete@dadevs.co.za>
2022-12-13 16:30:25 +0200

    updates

---
+++ client-angular/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./app/**/*.{html,js}'],
+  plugins: [require('daisyui')],
+  theme: {
+      container: {
+        center: true,
+      },
+      card:{
+          center:true,
+      }
+  },
+};
