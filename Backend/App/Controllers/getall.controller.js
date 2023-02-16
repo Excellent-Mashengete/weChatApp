07627b583823149fbf99260b0daee1e999207894
Excellent <excellent.mashengete@dadevs.co.za>
2023-02-16 20:00:44 +0200

    can view currently send messages

--- Backend/App/Controllers/getall.controller.js
+++ Backend/App/Controllers/getall.controller.js
@@ -16,73 +16,43 @@ module.exports.getUser = async (req, res) => {
 
 module.exports.getAllChatusers = async (req, res) => {
     const { sender_id } = req.params;
-    console.log(sender_id);
-    try {
-        //update the message and date fields
+   
         const contacts = await messages.findAll({
-            where: {
-              recipient_id: sender_id
+            where: { 
+                [Sequelize.Op.or]: [
+                    { user_id: sender_id,},
+                    {recipient_id: sender_id}
+                ]
             },
-            attributes: [
-                'messages',
-                [Sequelize.fn('max', Sequelize.col('chat_messages.createdAt')),'last_message_date'],
-            ],
-            include: [
-                {
-                  model: user,
-                }
-            ],
-            group:[
-                'user.id',
-                'messages'
-            ], 
-            order:[
-                ['last_message_date', 'DESC']
-            ],    
+            attributes: ['messages', 'createdAt'],
+            include: [{ model: user}],
+            order:[['createdAt', 'DESC']],    
             limit:1 
         });
 
         const groups = await groupmembers.findAll({
-            where: {
-                user_id: sender_id
-            },
-            include: [
-                {
-                    model:conversations,
-                    include:[
-                        {
-                            model: messages,
-                            attributes: [
-                                'id',
-                                'messages',
-                               [Sequelize.fn('max', Sequelize.col('conversation->chat_messages.createdAt')),'last_message_date'],
-                            ],
-                            order:[
-                                ['createdAt', 'DESC']
-                            ],
-                            include: [
-                                {    
-                                    model: user,
-                                }
-                            ],
-                        },
-                    ]
-                }
-            ],
-            group:[
-                'groupMembers.id',
-                'conversation.id',
-                'conversation->chat_messages.id',
-                'conversation->chat_messages->user.id'
-            ]
-            
+            where: { user_id: sender_id },
+            include: [{
+                model:conversations,
+                include:[{
+                    model: messages,
+                    order:[
+                        ['id', 'DESC']
+                    ],
+                    limit:1,
+                    include: [{    
+                        model: user,
+                    }]
+                }],
+            }],
         });
+        
         let allContacts= [];
 
         await contacts.forEach(contact => {
           let userdata = {
                 lastMessage: contact.dataValues.messages,
-                datesend: contact.dataValues.last_message_date,
+                datesend: contact.dataValues.createdAt,
                 id: contact.dataValues.user.id,
                 name: contact.dataValues.user.name,
                 cellphone: contact.dataValues.user.cellphone,
@@ -95,7 +65,7 @@ module.exports.getAllChatusers = async (req, res) => {
         await groups.forEach(group => {
           let contactGroup ={
                 lastMessage: group.dataValues.conversation.chat_messages[0]?.dataValues.messages,
-                datesend: group.dataValues.conversation.chat_messages[0]?.dataValues.last_message_date,
+                datesend: group.dataValues.conversation.chat_messages[0]?.dataValues.createdAt,
                 id: group.dataValues.id,
                 name: group.dataValues.conversation.name,
                 cellphone: group.dataValues.conversation.chat_messages[0]?.user.dataValues.cellphone,
@@ -106,11 +76,7 @@ module.exports.getAllChatusers = async (req, res) => {
             allContacts.push(contactGroup);
         })
 
-        res.status(200).json({ allContact: allContacts });
-
-    } catch (error) {
-        res.status(400).json({ error: "DB error" });
-    }
+        res.status(200).json({ allContact: allContacts});
 }
 
 module.exports.allMessages = async (req, res) => {
